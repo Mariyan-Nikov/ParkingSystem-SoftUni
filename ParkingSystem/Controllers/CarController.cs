704aecf50da1a4c68505e4ddb713cac3476b5aac
mniko <mniko@PC>
2024-04-14 16:13:31 +0300

    Add project files.

---
+++ ParkingSystem/Controllers/CarController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingSystem.Data;
+using ParkingSystem.Data.Models;
+
+namespace ParkingSystem.Controllers
+{
+    public class CarController : Controller
+    {
+        [HttpPost]
+        public IActionResult AddCar(Car car)
+        {
+            DataAccess.Cars.Add(car);
+            return Redirect("/");
+        }
+        [HttpPost]
+        public IActionResult DeleteCar(string plateNumber)
+        {
+            Car car = DataAccess.Cars.FirstOrDefault(x => x.PlateNumber == plateNumber);
+            DataAccess.Cars.Remove(car);
+            return Redirect("/");
+        }
+    }
+}
---
+++ ParkingSystem/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingSystem.Data;
+
+namespace ParkingSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(DataAccess.Cars);
+
+        }
+
+    }
+}
+
